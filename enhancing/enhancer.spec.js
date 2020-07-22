80bfbee90cb9edcced4ec3760aa6e4c30087b8be
Virginia Davenport <virginialdport@gmail.com>
2020-07-22 15:57:12 -0600

    MVP

--- enhancing/enhancer.spec.js
+++ enhancing/enhancer.spec.js
@@ -1,4 +1,4 @@
-const {repair} = require('./enhancer.js');
+const {repair, success, fail} = require('./enhancer.js');
 // test away!
 
 describe("repair()", function(){
@@ -18,4 +18,76 @@ describe("repair()", function(){
 
 
     }) 
+})
+
+describe("success()", function(){
+    it("should increase enhancement by 1", function(){
+        // let currentEnhancement = 17
+        let item = {
+            name: "arrow",
+            durability: 52,
+            enhancement: 15
+        }
+
+        const enhancedItem = success(item)
+
+        expect(enhancedItem.enhancement).toBe(16)
+    })
+
+    it("should not increase enhancement past 20", function(){
+        let item = {
+            name: "arrow",
+            durability: 52,
+            enhancement: 20
+        }
+
+        const enhancedItem = success(item)
+
+        expect(enhancedItem.enhancement).toBe(20)
+    })
+});
+
+describe("fail()", function(){
+    it("should decrease durability by 5 if enhancement is less than 15", function(){
+        let item = {
+            name: "arrow",
+            durability: 52,
+            enhancement: 14
+        }
+
+        const failItem = fail(item)
+
+        expect(failItem.durability).toBe(47)
+    })
+
+    it("should decrease durability by 10 if enhancement is >= 15", function(){
+        let item = {
+            name: "arrow",
+            durability: 50,
+            enhancement: 15
+        }
+        let itemPlus = {
+            name: "arrow2",
+            durability: 40,
+            enhancement: 18
+        }
+
+        const failItem = fail(item)
+        const failItemPlus = fail(itemPlus)
+
+        expect(failItem.durability).toBe(40)
+        expect(failItemPlus.durability).toBe(30)
+    })
+
+    it("should decrease enhancement by 1 if > 16", function(){
+        let item = {
+            name: "arrow2",
+            durability: 40,
+            enhancement: 18
+        }
+
+        const failItem = fail(item)
+
+        expect(failItem.enhancement).toBe(17)
+    })
 })
