a64cfddd66b30e2b09a64d652be1b07c31195bb5
Virginia Davenport <virginialdport@gmail.com>
2020-07-22 13:33:44 -0600

    repair() function and test appear to be working

--- enhancing/enhancer.spec.js
+++ enhancing/enhancer.spec.js
@@ -1,2 +1,21 @@
-const enhancer = require('./enhancer.js');
+const {repair} = require('./enhancer.js');
 // test away!
+
+describe("repair()", function(){
+    it("should restore durability to 100", () => {
+        //Arrange
+        let item ={
+            name:  "arrow",
+            durability: 52,
+            enhancement: 7
+        }
+        
+        //Act
+        const restoredItem = repair(item)
+
+        //Assert 
+        expect(restoredItem.durability).toBe(100)
+
+
+    }) 
+})
